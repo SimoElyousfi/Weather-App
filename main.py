e0c2503b1fbc2bb8e4e96cc5e7aa76cd4b5250ae
Mohammed El-yousfi <146152357+SimoElyousfi@users.noreply.github.com>
2023-12-07 13:58:27 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -9,19 +9,26 @@ api_key = "36a7f07130362f73bdce84ed965e9c36"
 # Call current weather data
 def fetch_data(city):
     global api_key
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     if response.status_code == requests.codes.ok:
         json_data = json.loads(response.content)
+        print(url)
         return json_data
 
 
 def get():
     global switch
     try:
-        temp = fetch_data(switch.get())["main"]["temp"]
-        description = fetch_data(switch.get())["weather"][0]["description"]
-        messagebox.showinfo(f"Weather in {switch.get()}", f"{temp} {description}")
+        temp = int(fetch_data(switch.get())["main"]["temp"])
+        humidity = int(fetch_data(switch.get())["main"]["humidity"])
+        description = fetch_data(switch.get())["weather"][0]["description"].title()
+
+        info = f"Temperature: {temp}C\n" \
+               f"Humidity: {humidity}\n"\
+               f"Weather description: {description}"
+
+        messagebox.showinfo(f"Weather in {switch.get()}", info)
     except TypeError as e:
         messagebox.showerror("Error", "Error fetching weather data, maybe city name incorrect")
 
